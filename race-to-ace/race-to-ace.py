38403e857799b80edea5d5d6a473ad634e1869c8
leote <tervittrades@gmail.com>
2025-09-20 15:42:09 +0100

    Add files via upload

---
+++ race-to-ace/race-to-ace.py
@@ -0,0 +1,24 @@
+import random
+
+def first_ace():
+    deck = list(range(1, 14)) * 4 # deck of cards
+    random.shuffle(deck)
+    
+    counter = 0
+    for card in deck:
+        counter += 1
+        if card == 1:  # if ace
+            return counter
+
+
+num_simulations = 100_000 # no. of simulations
+total_cards = 0
+
+for i in range(num_simulations):
+    total_cards += first_ace()
+
+average_cards = total_cards / num_simulations
+
+
+
+print("Average cards drawn = ", average_cards )
